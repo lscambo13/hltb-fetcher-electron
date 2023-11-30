631f0954e628ad1feceedc63808917d2fc37449c
lscambo13 <ls.cambo13@gmail.com>
2023-12-01 01:37:55 +0530

    Changes: hide menu bar update counter var names make function to get time save final log to disk as json save all detail to games individually catch errors in async code

--- main.js
+++ main.js
@@ -4,18 +4,31 @@ const fs = require('fs')
 const hltb = require('howlongtobeat')
 const hltbService = new hltb.HowLongToBeatService()
 
-
-const { ipcRenderer } = require('electron/renderer')
-
 process.env.NODE_ENV = 'production'
 
 const isDev = process.env.NODE_ENV === 'development'
 let mainWin;
 let flag = 'force';
 let gamesScanned = 0
-let gamesToScan = 0
-let foundGames = 0
-let notFoundGames = 0
+let totalGamesToScan = 0
+let counterGamesFound = 0
+let counterGamesNotFound = 0
+
+class gameObject {
+    constructor(name, dir, mainTime, extraTime, completeTime, url) {
+        this.gameName = name,
+            this.gameDir = dir,
+            this.gameTimes = [mainTime, extraTime, completeTime],
+            this.coverUrl = url
+    }
+}
+let allGames = []
+
+let failedGames = []
+let failedGameDirs = []
+let passedGames = []
+let passedGameDir = []
+
 
 const createMainWindow = () => {
     mainWin = new BrowserWindow({
@@ -28,11 +41,14 @@ const createMainWindow = () => {
             contextIsolation: true,
             sandbox: true
         },
-        transparent: true,
-        backgroundMaterial: 'mica',
+        // transparent: true,
+        // backgroundMaterial: 'mica',
+        autoHideMenuBar: true,
+
     })
     mainWin.loadFile(path.join(__dirname, 'renderer/index.html'))
     if (isDev) mainWin.webContents.openDevTools()
+    // console.log(giveMeTime())
 }
 
 app.whenReady().then(() => {
@@ -50,26 +66,60 @@ function sendMsg(msg, channel = 'LOG') {
 
 // #####################################
 
+const giveMeTime = () => {
+    let d = new Date()
+    let m = d.getMonth()
+    let t = d.toLocaleTimeString('en-us')
+    t = t.replace(':', '.')
+    t = t.replace(':', '.')
+    t = t.replace(' ', '.')
+    return `${d.getFullYear()}-${++m}-${d.getDate()} ${t}`
+}
+
+const logToDisk = () => {
+
+    allGames.forEach((val, index) => {
+        console.log(val)
+    })
+
+    let saveDir = path.join(process.env.HOME, `HLTB_Fetcher/`)
+    if (!fs.existsSync(saveDir)) fs.mkdirSync(saveDir)
+    saveDir = path.join(saveDir, giveMeTime())
+    fs.mkdirSync(saveDir)
+    fs.writeFileSync(path.join(saveDir, `/report.json`),
+        JSON.stringify(allGames)
+    )
+}
+
 const log = () => {
-    if (gamesScanned == gamesToScan) {
+    if (gamesScanned == totalGamesToScan) {
         sendMsg(`\n`)
         sendMsg(`# # # #   R E S U L T S   # # # #\n`)
-        sendMsg(`Total Games Scanned: ${gamesToScan}`)
-        sendMsg(`Success: ${foundGames}`)
-        sendMsg(`Could Not Find: ${notFoundGames}`)
+        sendMsg(`Total Games Scanned: ${totalGamesToScan}`)
+        sendMsg(`Success: ${counterGamesFound}`)
+        sendMsg(`Could Not Find: ${counterGamesNotFound}`)
         sendMsg(`# # # # # # # # # # # # # # # # #\n`)
         sendMsg('Quitting!' + '\n' + 'https://github.com/lscambo13/HLTB_Fetcher\n');
         // process.exit(0);
 
+        failedGames.forEach((val, index) => {
+            console.log(`FAILED ${val} - ${failedGameDirs[index]}`)
+        })
+
+        logToDisk()
+
         gamesScanned = 0
-        gamesToScan = 0
-        foundGames = 0
-        notFoundGames = 0
+        totalGamesToScan = 0
+        counterGamesFound = 0
+        counterGamesNotFound = 0
     }
 }
 
 let date = new Date()
 const saveInfo = (info, coverArt, address) => {
+
+    let gameEntry = new gameObject(info.name, address, info.gameplayMain, info.gameplayMainExtra, info.gameplayCompletionist, coverArt)
+    allGames.push(gameEntry)
 
     if (fs.existsSync(address) && flag == null) {
         sendMsg('SKIP: ' + info.name + ' - Enable overwrite mode to force refresh')
@@ -105,38 +155,46 @@ const saveInfo = (info, coverArt, address) => {
     let lineTwo = `Playing this game along with some side missions and activities should take about ${info.gameplayMainExtra} hours, and 100% completion can take around ${info.gameplayCompletionist} hours. `
     let lineThree = `\n\n`
     let lineFour = `This info was last updated on ${date.toUTCString()}.\nMore info about the tool used to fetch this info can be found on https://github.com/lscambo13/HLTB_Fetcher`
-
+    fs.writeFileSync(path.join(address, `/allInfo.json`), JSON.stringify(info))
     fs.writeFileSync(path.join(address, `/Main Story - ${info.gameplayMain} hours - ${safeName}.txt`), lineOne + lineTwo + lineThree + lineFour)
     fs.writeFileSync(path.join(address, `/Main & Sides - ${info.gameplayMainExtra} hours - ${safeName}.txt`), lineOne + lineTwo + lineThree + lineFour)
     fs.writeFileSync(path.join(address, `/Completionist - ${info.gameplayCompletionist} hours - ${safeName}.txt`), lineOne + lineTwo + lineThree + lineFour)
 }
 
 
-const findGame = (folder, dir) => {
-    ++gamesToScan
-    hltbService.search(folder).then((searchResults) => {
-        const quickSearch = () => {
-            return [searchResults[0].id, searchResults[0].imageUrl, folder]
-        }
+const findGame = async (folder, dir) => {
+    ++totalGamesToScan
 
+    hltbService.search(folder).then((searchResults) => {
         if (searchResults.length !== 0) {
-            let id = quickSearch()
-            hltbService.detail(id[0]).then((gameDetails) => {
-                foundGames++
+            // let id = quickSearch()
+            hltbService.detail(searchResults[0].id).then((gameDetails) => {
+                // console.log(gameDetails)
+                counterGamesFound++
                 ++gamesScanned
+                passedGames.push(folder)
+                passedGameDir.push(dir)
                 // sendMsg('FOUND: ' + `${gamesScanned}. ${folder}`)
-                saveInfo(gameDetails, id[1], path.join(dir, id[2], '/HowLongToBeat-Stats'))
+                saveInfo(gameDetails, searchResults[0].imageUrl, path.join(dir, folder, '/HowLongToBeat-Stats'))
                 log()
-            })
+            }).catch((err2) => console.log(`DETAIL ERROR: ${err2}`))
 
         } else {
-            notFoundGames++
+            counterGamesNotFound++
             ++gamesScanned
+            failedGames.push(folder)
+            failedGameDirs.push(dir)
             sendMsg(`ERROR: ${folder} - Check if the game folder name is spelled correctly`)
             // sendMsg('[INFO]\t\tCheck if the game folder name is spelled correctly')
             log()
         }
-    })
+    }).catch((err1) => console.log(`SEARCH ERROR: ${err1}`))
+    // const quickSearch = () => {
+    //     return [searchResults[0].id, searchResults[0].imageUrl, folder]
+    // }
+
+
+
 }
 
 let gamePaths = []
@@ -148,9 +206,13 @@ const startOnlineScan = () => {
 }
 
 const readDirs = (dir) => {
+    //
+    // reset folder arrays and Call readdir again in case folder has ben renamed!
+    // 
     let output = fs.readdirSync(dir, { withFileTypes: true })
     const ignored = (string) => {
-        let ignoredDirs = ['hjkdfjkhjhk', 'ig me', '$RECYCLE.BIN', 'System Volume Information', 'msdownld.tmp', '$Trash$'];
+        // 'hjkdfjkhjhk', 'ig me', 
+        let ignoredDirs = ['$RECYCLE.BIN', 'System Volume Information', 'msdownld.tmp', '$Trash$'];
         for (let val of ignoredDirs) {
             if (string.includes(val)) return true
         }
