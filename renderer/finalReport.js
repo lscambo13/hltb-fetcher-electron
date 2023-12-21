8d6d808981ce666756d5058b39dc2a8ce991ef2b
lscambo13 <ls.cambo13@gmail.com>
2023-12-22 01:19:09 +0530

    Changes in the final report design new: You can change between grid and list style new: We now have a side bar to show extra facts about your library new: We now highlight titles that show 0 hours dev: Sort and clone arrays (ascending, descending, alphabetically, top ten longest and shortest) dev: Handle top ten safely fix: Improved design consistency

--- renderer/finalReport.js
+++ renderer/finalReport.js
@@ -1,4 +1,6 @@
 const cardGallery = document.querySelector('.cardGallery')
+const gridViewButton = document.querySelector('#gridViewButton')
+const listViewButton = document.querySelector('#listViewButton')
 
 let gameLabels = []
 let gameDirs = []
@@ -9,14 +11,17 @@ let complete = []
 let gameArts = []
 
 const createCard = (img, title, times) => {
-	times.forEach((e, i) => {
+	let t = [...times]
+	let red = ['ok', 'ok', 'ok']
+	t.forEach((e, i) => {
+		if (t[i] == 0) red[i] = 'problem'
 		if (e % 1 != 0) {
-			times[i] = `${Math.floor(e)}<span class="timeFraction">
+			t[i] = `${Math.floor(e)}<span class="timeFraction">
 							<sup>1</sup>&frasl;<sub>2</sub>
 						</span>`
 		}
-
 	})
+
 	cardGallery.insertAdjacentHTML("beforeend", `
 				<div class="cardContainer">
 				<img class="cardImage"
@@ -27,22 +32,22 @@ const createCard = (img, title, times) => {
 					</div>
 					<div class="cardTimesContainer">
 					<div>
-						<div class="cardTime mainTimeOnly">
-							<p class="time">${times[0]}</p>
+						<div class="cardTime mainTimeOnly ${red[0]}">
+							<p class="time">${t[0]}</p>
 							<p class="timeUnit">hours</p>							
 						</div>
 						<p class="timeLabel">Main Only</p>
 					</div>
 					<div>
-						<div class="cardTime mainAndSides">
-							<p class="time">${times[1]}</p>
+						<div class="cardTime mainAndSides ${red[1]}">
+							<p class="time">${t[1]}</p>
 							<p class="timeUnit">hours</p>							
 						</div>
 						<p class="timeLabel">Main + Sides</p>
 					</div>
 					<div>
-						<div class="cardTime complete">
-							<p class="time">${times[2]}</p>
+						<div class="cardTime complete ${red[2]}">
+							<p class="time">${t[2]}</p>
 							<p class="timeUnit">hours</p>							
 						</div>
 						<p class="timeLabel">Completionist</p>
@@ -51,6 +56,80 @@ const createCard = (img, title, times) => {
 				</div>
 			</div>
 	`)
+}
+
+const addListStyle = () => {
+	listViewButton.disabled = true
+	gridViewButton.disabled = false
+	const cardContainer = document.querySelectorAll('.cardContainer')
+	const cardImage = document.querySelectorAll('.cardImage')
+	const cardData = document.querySelectorAll('.cardData')
+	const cardTitle = document.querySelectorAll('.cardTitle')
+	const cardTime = document.querySelectorAll('.cardTime')
+	const timeUnit = document.querySelectorAll('.timeUnit')
+
+
+	cardContainer.forEach(e => {
+		e.classList.remove('cardContainerGrid')
+		e.classList.add('cardContainerList')
+	})
+	cardImage.forEach(e => {
+		e.classList.remove('cardImageGrid')
+		e.classList.add('cardImageList')
+	})
+	cardData.forEach(e => {
+		e.classList.remove('cardDataGrid')
+		e.classList.add('cardDataList')
+	})
+	cardTitle.forEach(e => {
+		e.classList.remove('cardTitleGrid')
+		e.classList.add('cardTitleList')
+	})
+	cardTime.forEach(e => {
+		e.classList.remove('cardTimeGrid')
+		e.classList.add('cardTimeList')
+	})
+	timeUnit.forEach(e => {
+		e.classList.remove('timeUnitGrid')
+		e.classList.add('timeUnitList')
+	})
+}
+
+const addGridStyle = () => {
+	gridViewButton.disabled = true
+	listViewButton.disabled = false
+
+	const cardContainer = document.querySelectorAll('.cardContainer')
+	const cardImage = document.querySelectorAll('.cardImage')
+	const cardData = document.querySelectorAll('.cardData')
+	const cardTitle = document.querySelectorAll('.cardTitle')
+	const cardTime = document.querySelectorAll('.cardTime')
+	const timeUnit = document.querySelectorAll('.timeUnit')
+
+	cardContainer.forEach(e => {
+		e.classList.remove('cardContainerList')
+		e.classList.add('cardContainerGrid')
+	})
+	cardImage.forEach(e => {
+		e.classList.remove('cardImageList')
+		e.classList.add('cardImageGrid')
+	})
+	cardData.forEach(e => {
+		e.classList.remove('cardDataList')
+		e.classList.add('cardDataGrid')
+	})
+	cardTitle.forEach(e => {
+		e.classList.remove('cardTitleList')
+		e.classList.add('cardTitleGrid')
+	})
+	cardTime.forEach(e => {
+		e.classList.remove('cardTimeList')
+		e.classList.add('cardTimeGrid')
+	})
+	timeUnit.forEach(e => {
+		e.classList.remove('timeUnitList')
+		e.classList.add('timeUnitGrid')
+	})
 }
 
 fetch('./assets/database.json').then(res => {
@@ -62,17 +141,67 @@ fetch('./assets/database.json').then(res => {
 }).catch(e => {
 	console.log(e)
 	console.log('Did not find local database')
+	addMainListeners()
 	const restartAppButton = document.querySelector('#restartAppButton')
 	restartAppButton.addEventListener('click', () => {
 		bridgeApi.invoke('openRequest', 'restartApp')
 	})
-	addMainListener()
 	bridgeApi.invoke('endGameRequest', 'requestAllGameData')
 })
 
+const generateSortedArrays = (unorderedArray, order) => {
+	let x;
+	if (order == 'asc') {
+		let ascOrderedMainDb = [...unorderedArray]
+		ascOrderedMainDb.sort((a, b) => { return a.gameTimes[0] - b.gameTimes[0] })
+
+		let ascOrderedMainSidesDb = [...unorderedArray]
+		ascOrderedMainSidesDb.sort((a, b) => { return a.gameTimes[1] - b.gameTimes[1] })
+
+		let ascOrderedCompleteDb = [...unorderedArray]
+		ascOrderedCompleteDb.sort((a, b) => { return a.gameTimes[2] - b.gameTimes[2] })
+
+		x = [ascOrderedMainDb, ascOrderedMainSidesDb, ascOrderedCompleteDb]
+	} else if (order == 'desc') {
+		let descOrderedMainDb = [...unorderedArray]
+		descOrderedMainDb.sort((a, b) => { return b.gameTimes[0] - a.gameTimes[0] })
+
+		let descOrderedMainSidesDb = [...unorderedArray]
+		descOrderedMainSidesDb.sort((a, b) => { return b.gameTimes[1] - a.gameTimes[1] })
+
+		let descOrderedCompleteDb = [...unorderedArray]
+		descOrderedCompleteDb.sort((a, b) => { return b.gameTimes[2] - a.gameTimes[2] })
+
+		x = [descOrderedMainDb, descOrderedMainSidesDb, descOrderedCompleteDb]
+	}
+	return x
+}
+
+const getTopTen = (arrayOfArrays) => {
+	let list = document.querySelector('#topLongestList')
+	arrayOfArrays.forEach((element, arrIndex) => {
+		console.log('-----Looking at ', arrIndex)
+		let totalTries = 10
+		for (let i = 0; i < totalTries; i++) {
+			if (element[i].gameTimes[arrIndex]) {
+				list.insertAdjacentHTML("beforeend", `
+						<li class="sideListItems">${element[i].gameName}</li>
+						<span class="timeUnit">${element[i].gameTimes[arrIndex]}</span>
+				`)
+				// console.log(element[i].gameName, element[i].gameTimes[arrIndex])
+			} else { totalTries++ }
+		}
+	})
+}
+
 const beginConstructingDOM = (database) => {
 	console.log(database)
-	database.forEach((element, index) => {
+	const asciiOrderedDB = [...database]
+	asciiOrderedDB.sort((a, b) => {
+		if (a.gameName < b.gameName) { return -1 } else return 1
+	})
+
+	asciiOrderedDB.forEach((element, index) => {
 		let img = element.gameDir + '\\coverArt.jpg'
 		let title = element.gameName
 		let times = element.gameTimes
@@ -85,8 +214,11 @@ const beginConstructingDOM = (database) => {
 		gameArts.push(element.coverUrl)
 		createCard(img, title, times)
 	});
-	// console.log(mainOnly)
+	addGridStyle()
 
+	const ascSortedArrays = generateSortedArrays(asciiOrderedDB, 'asc')
+	const descSortedArrays = generateSortedArrays(asciiOrderedDB, 'desc')
+	getTopTen(descSortedArrays)
 
 	let allGameTimesLineGraphCFG = {
 		type: 'line',
@@ -245,10 +377,12 @@ const beginConstructingDOM = (database) => {
 	document.body.classList.remove('displayNone')
 }
 
-const addMainListener = () => {
+const addMainListeners = () => {
 	bridgeApi.on('ALL_GAMES', (args) => {
 		// console.log(args)
 		beginConstructingDOM(args)
 	})
+	gridViewButton.addEventListener('click', addGridStyle)
+	listViewButton.addEventListener('click', addListStyle)
 }
 
